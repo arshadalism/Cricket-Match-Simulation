0a7335460850bbcae2a3aa0e303f7598e700173b
Arshad <chaudharyarshad548@gmail.com>
2024-02-03 16:15:46 +0530

    bolwers scorecard

--- main.py
+++ main.py
@@ -85,16 +85,16 @@ class Bowler(Batsman):
 
         self.economy = (self.run_conceded / int(self.bowl_bowled)) * 6  # Update economy based on overs
 
-        print(f"{runs} runs bowled!")
+        # print(f"{runs} runs bowled!")
 
     def take_wicket(self):
         super().dismissed()
         self.wickets_taken += 1
-        print("Wickets taken")
+        # print("Wickets taken")
 
     def overs_and_balls(self, balls_bowled):
-        overs = balls_bowled // 6
-        balls_left = balls_bowled % 6
+        overs = int(balls_bowled // 6)
+        balls_left = int(balls_bowled % 6)
         return f"{overs}.{balls_left}"
 
     def display_bowling_stats(self):
@@ -114,6 +114,54 @@ def format_deliveries(all_deliveries, team_name):
     return team_name + " : " + " | ".join(formatted_data)
 
 
+def batsmen_scorecard(batsmen_stats, team_name):
+    scorecard_headers = ['Name', 'Runs', 'Balls', '4', '6', 'Strike Rate']
+    # scorecard_data = [[batsman, stats['Runs'], stats['Balls'], stats['4'], stats['6']] for
+    #                   batsman, stats in batsmen_stats.items()]
+
+    scorecard_data = []
+
+    for batsman, stats in batsmen_stats.items():
+        runs = stats['Runs']
+        balls = stats['Balls']
+        fours = stats['4']
+        sixes = stats['6']
+
+        if balls > 0 and runs > 0:
+            strike_rate = (runs / balls) * 100
+        else:
+            strike_rate = 0.000
+
+        scorecard_data.append([batsman, runs, balls, fours, sixes, f"{strike_rate:.2f}"])
+
+    print(f"\n---{team_name} Batsman's Scorecard ---")
+    print(tabulate(scorecard_data, headers=scorecard_headers))
+    print("-------------------")
+
+
+def bowlers_scorecard(bowlers_stats, team_name):
+    scorecard_headers = ['Name', 'Overs', 'Runs', 'Wickets', 'Economy']
+    scorecard_data = []
+
+    for bowler, stats in bowlers_stats.items():
+        overs = stats['Overs']
+        over = (overs * 10) // 6
+        balls = (overs * 10) % 6
+        runs = stats['Runs']
+        wickets = stats['Wickets']
+
+        if overs > 0 and runs > 0:
+            economy = (runs / (overs * 10)) * 6
+        else:
+            economy = 0.0
+
+        scorecard_data.append([bowler, f"{over:.0f}.{int(balls):.0f}", runs, wickets, f"{economy:.2f}"])
+
+    print(f"\n--- {team_name} Bowler's Scorecard ---")
+    print(tabulate(scorecard_data, headers=scorecard_headers))
+    print("-------------------")
+
+
 def innings_play(batter_list, bowler_list, overs, target=None):
     batsman_list = [Batsman(name) for name in batter_list]
 
@@ -131,7 +179,8 @@ def innings_play(batter_list, bowler_list, overs, target=None):
 
     all_deliveries = []
 
-    batsmen_stats = {batsman.name: {'Runs': 0, 'Balls': 0, '4': 0, '6': 0, 'Strike Rate': 0.0} for batsman in batsman_list}
+    batsmen_stats = {batsman.name: {'Runs': 0, 'Balls': 0, '4': 0, '6': 0} for batsman in batsman_list}
+    bowlers_stats = {bowler.name: {'Overs': 0, 'Runs': 0, 'Wickets': 0} for bowler in bowlers}
 
     for _ in range(overs * 6):
         runs = random.choice([1, 2, 3, 4, 6, 'W'])
@@ -148,7 +197,16 @@ def innings_play(batter_list, bowler_list, overs, target=None):
 
         if target is not None and team_score >= target:
             overs_played = total_ball // 6 + total_ball % 6 / 10
+            # scorecard_headers = ['Name', 'Runs', 'Balls', '4', '6', 'Strike Rate']
+            # scorecard_data = [[batsman, stats['Runs'], stats['Balls'], stats['4'], stats['6'], stats['Strike Rate']] for
+            #                   batsman, stats in batsmen_stats.items()]
+            # print("\n--- Batsman's Scorecard ---")
+            # print(tabulate(scorecard_data, headers=scorecard_headers))
+            # print("-------------------")
+            batsmen_scorecard(batsmen_stats, "Australia")
+            bowlers_scorecard(bowlers_stats, "India")
             return team_score, wickets_lost, float(overs_played), all_deliveries
+
 
         all_deliveries.append(runs)
 
@@ -163,23 +221,26 @@ def innings_play(batter_list, bowler_list, overs, target=None):
 
         if runs == 'W':
             current_batsman_strike.dismissed()
+            # bowlers_stats[current_bowler.name]['Wickets'] += 1
             if wickets_lost < 10:
                 wickets_lost += 1
-                print(f"{current_batsman_strike.name} got out.")
-                print(f"runs: {current_batsman_strike.run_scored}, ball_played: {current_batsman_strike.bowl_played}")
+                # print(f"{current_batsman_strike.name} got out.")
+                # print(f"runs: {current_batsman_strike.run_scored}, ball_played: {current_batsman_strike.bowl_played}")
                 batsmen_stats[current_batsman_strike.name]['Balls'] += 1
+                bowlers_stats[current_bowler.name]['Wickets'] += 1
+                bowlers_stats[current_bowler.name]['Overs'] += 0.1
 
                 next_batsman = next((batsman for batsman in batsman_list[next_batsman_index:] if
                                      not batsman.status and batsman != current_batsman_strike and batsman != current_batsman_non_strike),
                                     None)
                 if next_batsman:
-                    print(f"Next batsman: {next_batsman.name}")
+                    # print(f"Next batsman: {next_batsman.name}")
                     batsman_list[i_strike] = next_batsman
                     next_batsman_index += 1
                     i_strike = (i_strike % 2) % len(batsman_list)
                     # i_non_strike = (i_strike + 1) % len(batsman_list)
                 else:
-                    print("All batsmen are out")
+                    # print("All batsmen are out")
                     break
         # if runs != 'W':
         #     team_score += runs
@@ -189,6 +250,8 @@ def innings_play(batter_list, bowler_list, overs, target=None):
 
         if runs != 'W':
             team_score += runs
+            current_bowler.over_bowled += 0.1
+            bowlers_stats[current_bowler.name]['Runs'] += runs
             batsmen_stats[current_batsman_strike.name]['Runs'] += runs
             batsmen_stats[current_batsman_strike.name]['Balls'] += 1
             if runs == 4:
@@ -196,11 +259,24 @@ def innings_play(batter_list, bowler_list, overs, target=None):
             elif runs == 6:
                 batsmen_stats[current_batsman_strike.name]['6'] += 1
 
-        print("------Batting Striker--------")
-        current_batsman_strike.display_batting_stats()
+            # current_bowler.over_bowled += 0.1
+            # bowlers_stats[current_bowler.name]['Runs'] += runs
 
-        print("-----Batting Non striker--------")
-        current_batsman_non_strike.display_batting_stats()
+            # overs_bowled, balls_bowled = current_bowler.overs_and_balls(current_bowler.bowl_bowled)
+            bowlers_stats[current_bowler.name]['Overs'] += 0.1
+            bowlers_stats[current_bowler.name]['Wickets'] = current_bowler.wickets_taken
+
+            if current_bowler.bowl_bowled > 0:
+                bowlers_stats[current_bowler.name]['Economy'] = (
+                                                                            current_bowler.run_conceded / current_bowler.bowl_bowled) * 6
+            else:
+                bowlers_stats[current_bowler.name]['Economy'] = 0.0
+
+        # print("------Batting Striker--------")
+        # current_batsman_strike.display_batting_stats()
+
+        # print("-----Batting Non striker--------")
+        # current_batsman_non_strike.display_batting_stats()
 
         if (current_bowler.bowl_bowled % 6 != 0) and runs in [1, 3]:
             i_strike, i_non_strike = i_non_strike, i_strike
@@ -208,26 +284,36 @@ def innings_play(batter_list, bowler_list, overs, target=None):
         # print("-----Batting Non striker--------")
         # current_batsman_non_strike.display_batting_stats()
 
-        print('---------Bowling---------')
-        current_bowler.display_bowling_stats()
+        # print('---------Bowling---------')
+        # current_bowler.display_bowling_stats()
 
         if (current_bowler.bowl_bowled % 6 == 0) and (runs not in [1, 3]):
             i_strike, i_non_strike = i_non_strike, i_strike
 
         if current_bowler.bowl_bowled % 6 == 0:
             i += 1
-        print("Done")
-        print("------------------------")
+        # print("Done")
+        # print("------------------------")
 
-    scorecard_headers = ['Name', 'Runs', 'Balls', '4', '6', 'Strike Rate']
-    scorecard_data = [[batsman, stats['Runs'], stats['Balls'], stats['4'], stats['6'], stats['Strike Rate']] for
-                      batsman, stats in batsmen_stats.items()]
-    print("\n--- Batsmen's Scorecard ---")
-    print(tabulate(scorecard_data, headers=scorecard_headers))
-    print("-------------------")
+    # scorecard_headers = ['Name', 'Runs', 'Balls', '4', '6', 'Strike Rate']
+    # scorecard_data = [[batsman, stats['Runs'], stats['Balls'], stats['4'], stats['6'], stats['Strike Rate']] for
+    #                   batsman, stats in batsmen_stats.items()]
+    # print("\n--- Batsman's Scorecard ---")
+    # print(tabulate(scorecard_data, headers=scorecard_headers))
+    # print("-------------------")
+    if target:
+        batsmen_scorecard(batsmen_stats, "Australia")
+        bowlers_scorecard(bowlers_stats, "India")
+        overs_played = total_ball // 6 + total_ball % 6 / 10
 
-    overs_played = total_ball // 6 + total_ball % 6 / 10
-    return team_score, wickets_lost, float(overs_played), all_deliveries
+        return team_score, wickets_lost, float(overs_played), all_deliveries
+    else:
+
+        batsmen_scorecard(batsmen_stats, "India")
+        bowlers_scorecard(bowlers_stats, "Australia")
+
+        overs_played = total_ball // 6 + total_ball % 6 / 10
+        return team_score, wickets_lost, float(overs_played), all_deliveries
 
 
 if __name__ == '__main__':
@@ -256,6 +342,8 @@ if __name__ == '__main__':
         print(f"Australia won the match by {10 - second_inning[1]} wickets.")
     else:
         print("It's a tie! score level")
+
+    print("\n--- Over Summary ---")
 
     print(format_deliveries(first_inning[3], "India"))
     print(format_deliveries(second_inning[3], "Australia"))
